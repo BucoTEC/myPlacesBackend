26c406c541a8eade6bd74afb2e33cc8d8f94a33d
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-01-26 21:37:56 +0100

    prepared for deployment

--- util/location.js
+++ util/location.js
@@ -1,33 +1,34 @@
-const axios = require('axios');
+const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
+const HttpError = require("../models/http-error");
 
-const HttpError = require('../models/http-error');
-
-const API_KEY = 'AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA';
-
+const mapToken = process.env.MAPBOX_TOKEN;
 async function getCoordsForAddress(address) {
   // return {
   //   lat: 40.7484474,
   //   lng: -73.9871516
   // };
   const response = await axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-      address
-    )}&key=${API_KEY}`
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/sarajevo.json?types=place%2Cpostcode%2Caddress&access_token=${mapToken}`
   );
 
   const data = response.data;
 
-  if (!data || data.status === 'ZERO_RESULTS') {
+  if (!data || data.status === "ZERO_RESULTS") {
     const error = new HttpError(
-      'Could not find location for the specified address.',
+      "Could not find location for the specified address.",
       422
     );
     throw error;
   }
 
-  const coordinates = data.results[0].geometry.location;
+  const coordinates = data.features[0].geometry.coordinates;
 
-  return coordinates;
+  return {
+    lat: coordinates[1],
+    lng: coordinates[0],
+  };
 }
 
 module.exports = getCoordsForAddress;
